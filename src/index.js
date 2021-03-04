36142a2d0072d97e7b7a8796bbebeb0aa1b2b692
coolmanben <mZmiterizrigi@gmail.com>
2021-03-05 02:46:14 +0300

    init: start task typical-arrays-problems
    init: start task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.max = function min (array) {
+  var res = 0 ;
+  if ( (array.length > 0 ) && ( typeof array != 'undefined') ) {  
+    res = Math.max.apply(null, array);
+  }                     
+  return res; 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function max (array) {
+  var res = 0 ;
+  if ( (array.length > 0 ) && ( typeof array != 'undefined') ) {  
+    res = Math.min.apply(null, array);
+  }                     
+  return res; 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var sum = 0;
+  var res = 0;
+  if ( (array.length > 0 ) && ( typeof array != 'undefined') ) { 
+      for (var i = 0; i < array.length; i++) {
+          sum = sum + array[i];
+      }
+    res = sum / array.length;
+  }
+ return res;
 }
